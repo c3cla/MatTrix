db2619b2b583c5a06cbfa8b701abd0b1fa78f043
Cecla <vicente.yagello@hotmail.com>
2024-09-24 10:37:22 -0300

    Notas en el home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,84 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+import Note from "../components/Note"
+import "../styles/Home.css"
+
 function Home() {
-    return <div>Home</div>
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Nota eliminada!");
+        else alert("Error al eliminar la nota");
+        getNotes();
+      })
+      .catch((error) => alert(error));
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then((res) => {
+        if (res.status === 201) alert("Nota creada con exito!");
+        else alert("Error al crear la nota");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Notas</h2>
+        {notes.map((note) => (
+          <Note note={note} onDelete={deleteNote} key={note.id} />
+        ))}
+      </div>
+      <h2>Crear Nota</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Titulo:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="content">Contenido:</label>
+        <br />
+        <textarea
+          id="content"
+          name="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
